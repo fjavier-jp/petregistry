dd5bd25d8c589742c3e5cde102047cd89b35ddfe
Francisco Javier Jiménez Paredes <fjavierjp@protonmail.com>
2025-04-09 20:21:52 +0200

    Initial commit

---
+++ src/test/java/org/hopto/fjavierjp/petregistry/PetregistryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hopto.fjavierjp.petregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetregistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
